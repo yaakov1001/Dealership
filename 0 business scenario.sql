cd8ed83a0160d4ad56452b475b64846b4a5c40ab
yaakov1001 <yaakovkrupnick@gmail.com>
2024-09-10 14:15:21 -0400

    Business Scenario

---
+++ 0 business scenario.sql
@@ -0,0 +1,37 @@
+/*
+We have a car dealership that sells and leases many models all off one make - Chevrolet.
+We specify each car by its model, year, and color and we keep a record of when each car was purchased and for how much.
+We record whether it was sold or leased and for how much.
+The price of a lease is the total of the payments received for the lease period.
+Once purchased, we want to enter the customer's information - first name, last name, and phone number -into the system.
+We only deal with new vehicles at this site and when the leases are returned they are resold at another site.
+*/
+
+/*
+Reports:
+How many cars did we sell and how many cars did we lease?
+
+Which model earned the most revenue?
+
+For the cars that were sold/leased how long did it take on average for the transaction to occur after it was purchased by the dealership?
+
+Which color had the most sales/leases?
+*/
+
+/*
+Questions:
+Q: Would the dealership ever purchase a car from the previous year to sell?
+A: No, if we have it left in our inventory, we would sell it, but initially we would only purchase this year's or next year's model.
+In any case, we wouldn't sell a car more than five years old.
+
+Q: Is it possible to purchase a future year's model?
+A; Yes, next year's model usually is available towards the end of the current year,
+but obviously not the model for two or more years in advance.
+
+Q:What is the highest and lowest price you would sell a car?
+A:We haven't sold a car for less than $20,000 or leased for less than $10000 in a long time and prices are only going up.
+As far as the maximum, we plan to be in business for years to come and it's hard to know what prices will be like.
+
+Q:Do you need to keep on record the customer's address as well?
+A: No.
+*/
