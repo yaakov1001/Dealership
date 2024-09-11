083160587312a8b8b62eb73fcc72fe045262c067
yaakov1001 <yaakovkrupnick@gmail.com>
2024-09-11 12:40:01 -0400

    sample data

---
+++ sample data.sql
@@ -0,0 +1,14 @@
+/*
+'Trailblazer', 2025, 'gray', '08-01-24', 18000, null, null, null
+'Trailblazer', 2024, 'black', '01-02-24', 18000, null, null, null
+'Trailblazer', 2023, 'blue', '10-01-22', 17500, '06-01-23', 20300, 'leased'
+'Trailblazer', 2022, 'white', '11-01-21', 17000, '05-10-22', 30000, 'sold'
+'Trax', 2025, 'gray', '08-01-24', 17000, '08-15-24', 28000, 'sold'
+'Trax', 2020, 'red', '04-01-20',16000, '03-01-21', 21300, 'leased'
+'Silveradoo', 2024, 'gray', '02-01-24', 24000, '05-10-24', 35000, 'sold'
+'Silveradoo', 2024, 'red', '10-20-23', 24000, '06-12-24', 37000, 'sold'
+'Silveradoo', 2020, 'white', '03-01-20', 23000, null, null, null
+'Malibu', 2024, 'gray', '09-01-23', 15000, '01-23-24', 18200, 'leased'
+'Corvette', 2025, 'orange', '08-01-24', 45000, null, null, null
+'Camaro', 2022, 'gray', '02-01-22', 40000, '10-08-22', 60000, 'sold'
+*/
